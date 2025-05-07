35a93340b302944a331e512f87102ccb6a483c2d
MaryCabezas <marycielo.cabezas@unas.edu.pe>
2025-05-07 08:22:55 -0500

    Guardando cambios locales antes del pull

--- db/migrate/20250419235424_add_servicio_id_to_samples.rb
+++
@@ -1,5 +0,0 @@
-class AddServicioIdToSamples < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :samples, :servicio, null: false, foreign_key: true
-  end
-end
--- db/migrate/20250420000500_add_extra_fields_to_samples.rb
+++
@@ -1,8 +0,0 @@
-class AddExtraFieldsToSamples < ActiveRecord::Migration[7.0]
-  def change
-    # Ya existen: :fecha_recepcion, :prioridad, :observaciones
-    # Solo agregar lo que falta:
-    add_column :samples, :fecha_resultado, :date
-    add_column :samples, :estado, :string
-  end
-end
